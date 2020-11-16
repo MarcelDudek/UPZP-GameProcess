b813d46ca422f74489f39ab1f96157f7dd3bd69a
Marcel Dudek <43888122+MarcelDudek@users.noreply.github.com>
2020-11-16 07:35:24 +0100

    Include flatbuffers and asio libraries into the project (#1)
    - added asio library
    - added flatbuffers library

--- UPZP-GameProcess/main.cpp
+++ UPZP-GameProcess/main.cpp
@@ -1,6 +1,11 @@
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+#define ASIO_STANDALONE
+
 #include <iostream>
 
-int main() { 
-  std::cout << "Hello world!" << std::endl;
-  return 0; 
+int main() {
+  std::cout << "Hello world!";
+  return 0;
 }
