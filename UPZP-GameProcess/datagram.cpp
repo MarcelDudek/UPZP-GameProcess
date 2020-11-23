e5c35256dc9defcd04e1cf912bb505b14029c84c
Marcel Dudek <lotnik1998@gmail.com>
2020-11-23 23:04:57 +0100

    Loading datagram from buffor
    Load method for Datagram class, not tested yet.

---
+++ UPZP-GameProcess/datagram.cpp
@@ -0,0 +1,63 @@
+#include "datagram.h"
+#include <stdexcept>
+#include <string>
+
+namespace upzp {
+
+/**
+ * @brief Load the datagram from buffor.
+ * @param buffor 
+ * @param lenght Length of the buffor.
+*/
+void Datagram::Load(const void* buffor, const size_t length) {
+  constexpr size_t MINIMAL_LENGTH = 12;
+
+  if (length < MINIMAL_LENGTH) {
+    throw std::runtime_error("Datagram length " + std::to_string(length) +
+                             " is shorter than minimal length of " +
+        std::to_string(MINIMAL_LENGTH) + ".");
+  }
+
+  // load begin sequence
+  uint16_t begin_seq = *static_cast<const uint16_t*>(buffor);
+  if (begin_seq != BEGIN_SEQUENCE) {
+    throw std::runtime_error("Begin sequence of datagram isn't correct.");
+  }
+
+  // load version and check if payload checksum is included
+  version_ = *(static_cast<const int16_t*>(buffor) + 2) & 0x00FF;
+  payload_checksum_included_ =
+      *(static_cast<const uint8_t*>(buffor) + 3) & 0b10000000;
+  payload_lenght_ = *(static_cast<const uint32_t*>(buffor) + 4);
+
+  unsigned short header_lenght;
+  int16_t header_checksum;
+  const char* payload_start;
+
+  // load checksums and payload length
+  if (payload_checksum_included_) {  // if payload checksum is inluded
+    if (length < MINIMAL_LENGTH + 4) {
+      throw std::runtime_error("Datagram length " + std::to_string(length) +
+                               " is shorter than length of header with payload checksum included" +
+                               std::to_string(MINIMAL_LENGTH + 4) + ".");
+    }
+
+    payload_checksum_ = *(static_cast<const uint32_t*>(buffor) + 8);
+    header_checksum = *(static_cast<const int16_t*>(buffor) + 14);
+    payload_start = static_cast<const char*>(buffor) + 16;
+    header_lenght = 16;
+  } else {  // if payload checksum isn't included
+    header_checksum = *(static_cast<const int16_t*>(buffor) + 10);
+    payload_start = static_cast<const char*>(buffor) + 12;
+    header_lenght = 12;
+  }
+
+  // load payload
+  if (payload_lenght_ + header_lenght > length) {
+    throw std::runtime_error("Payload length overflows the datagram.");
+  }
+  payload_ =
+      std::vector<const char>(payload_start, payload_start + payload_lenght_);
+}
+
+}  // namespace upzp
---
+++ UPZP-GameProcess/datagram.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+
+namespace upzp {
+
+class Datagram {
+ private:
+  static constexpr auto BEGIN_SEQUENCE = 0xABDA;
+
+  int16_t version_;
+  uint32_t payload_lenght_;
+  int32_t payload_checksum_;
+  bool payload_checksum_included_;
+
+  std::vector<const char> payload_;
+
+ public:
+  void Load(const void* buffor, const size_t length);
+};
+
+}  // namespace upzp
